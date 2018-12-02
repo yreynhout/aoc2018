f36702d8d37320e7b10b2f3f2c62a6fa64708035
Yves Reynhout <yves.reynhout@gmail.com>
2018-12-02 12:01:58 +0100

    day1 completed

---
+++ day1/part1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace day1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine
+            (
+                File
+                    .ReadAllLines(args[0])
+                    .Select(int.Parse)
+                    .Aggregate(0, (frequency, change) => frequency + change)
+            );
+        }
+    }
+}
---
+++ day1/part2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace day1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var input =  File
+                .ReadAllLines(args[0])
+                .Select(int.Parse)
+                .ToList();
+            var index = new HashSet<int>();
+            
+            var frequency = 0;
+            index.Add(frequency);
+            var frequency_found_twice = new Nullable<int>();
+            while(!frequency_found_twice.HasValue)
+            {
+                using(var enumerator = input.GetEnumerator())
+                {
+                    while(!frequency_found_twice.HasValue && enumerator.MoveNext())
+                    {
+                        frequency += enumerator.Current;
+                        if(index.Contains(frequency))
+                        {
+                            frequency_found_twice = frequency;
+                        } else {
+                            index.Add(frequency);
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(frequency_found_twice.Value);
+        }
+    }
+}
